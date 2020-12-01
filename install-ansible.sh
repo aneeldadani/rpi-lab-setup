c8ed9aaf53178fe57072b8fba5c7ae1d387c6aa8
Aneel <40775413+aneelysis@users.noreply.github.com>
2020-11-30 16:34:22 -0800

    Add files via upload

---
+++ install-ansible.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Updating apt repo"
+sudo apt update -y
+
+#echo "Sleeping for 30 seconds"
+#sleep 30
+
+echo "Installing software-properties-common"
+sudo apt install software-properties-common -y
+#echo "Sleeping for 30 seconds"
+#sleep 30
+
+echo "Adding Ansible repo"
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+#echo "Sleeping for 30 seconds"
+#sleep 30
+
+echo "Installing Ansible"
+sudo apt install ansible -y
+
+echo "Installation Complete"
+echo "Installed Ansible version $(ansible --version)"
